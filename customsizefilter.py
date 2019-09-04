fe191bfd6b46d0606b285a4bdef1814a09b424b1
mslichao <mslichao@outlook.com>
2019-09-04 14:16:40 +0800

    use ori size first

--- customsizefilter.py
+++ customsizefilter.py
@@ -9,17 +9,22 @@ def processCustomImageSize(key, value, _format, _meta):
     [attrs, _inline, _target] = value
     width = height = cw = ch = None
     for attr in attrs[2]:
+      if attr[0] == 'width':
+        width = attr[1]
+      if attr[0] == 'height':
+        height = attr[1]
       if attr[0] == 'cw':
         cw = attr[1]
       if attr[0] == 'ch':
         ch = attr[1]
-    if cw or ch:
-      newAttrs = []
-      if cw:
-        newAttrs.append(['width', cw])
-      if ch:
-        newAttrs.append(['height', ch])
-      attrs[2] = newAttrs
+    if not width and not height:
+      if cw or ch:
+        newAttrs = []
+        if cw:
+          newAttrs.append(['width', cw])
+        if ch:
+          newAttrs.append(['height', ch])
+        attrs[2] = newAttrs
 
 
 if __name__ == "__main__":
