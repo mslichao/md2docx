b00b32b9b8968388171b45a1654cece423a8230d
mslichao <mslichao@outlook.com>
2019-11-06 16:09:01 +0800

    dump

---
+++ dumpTableImage.py
@@ -0,0 +1,79 @@
+import argparse
+import os
+import sys
+import subprocess
+from typing import Iterator
+from helper import makesurePandoc
+import json
+import warnings
+
+
+def process(inputfile):
+  pandoc = makesurePandoc()
+  pandocReadAll = [pandoc, '--filter', 'customfilter.py', inputfile, '-t', 'json']
+  jsonString = subprocess.run(pandocReadAll, stdout=subprocess.PIPE).stdout
+
+  originDoc = json.loads(jsonString)
+  originBlocks = originDoc['blocks']
+
+  tableBlocks = []
+  imageBlocks = []
+
+  for (index, item) in enumerate(originBlocks):
+    # print(item)
+    if item['t'] == 'Table':
+      tableBlocks.append(originBlocks[index - 1])
+    if item['t'] == 'Para' and item['c'][0]['t'] == 'Image':
+      imageBlocks.append(originBlocks[index + 1])
+      
+  checkListOrder(tableBlocks, '表')
+  checkListOrder(imageBlocks, '图')
+
+  saveList(pandoc, originDoc, tableBlocks, 'table.txt')
+  saveList(pandoc, originDoc, imageBlocks, 'image.txt')
+
+
+def saveList(pandoc, originDoc, blocks, outputfile):
+  originDoc['blocks'] = blocks
+  jsonString = json.dumps(originDoc)
+  pandocWriter = [pandoc, '-f', 'json', '-o', outputfile]  
+  subprocess.run(pandocWriter, input=bytes(jsonString, encoding='utf-8'))
+
+
+def checkListOrder(blocks, preWord):
+  clipCount = 100
+  chap = 0
+  indexInChap = 0
+  for item in blocks:
+    # print(item)
+    try:
+      title = str(item['c'][0]['c'])
+      if not title.startswith(preWord):
+        warnings.warn(f'"{preWord}" title not starts with "{preWord}": {title[:clipCount]}')
+        continue
+      title = title[1:]
+      chapAndIndex = title.split('-')
+      if int(chapAndIndex[1]) == 1:
+        if int(chapAndIndex[0]) <= chap:
+          warnings.warn(f'"{preWord}" chap number error: {title[:clipCount]}')
+        chap = int(chapAndIndex[0])
+        indexInChap = int(chapAndIndex[1])
+      else:
+        if chap != int(chapAndIndex[0]) or indexInChap != (int(chapAndIndex[1]) - 1):
+          warnings.warn(f'"{preWord}" chap number error: {title[:clipCount]}')
+        chap = int(chapAndIndex[0])
+        indexInChap = int(chapAndIndex[1])
+
+    except Exception:
+      warnings.warn(f'"{preWord}" Failed when process {str(item)[:clipCount]}')
+
+  
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('inputfile',
+                      help='markdown file')
+  args = parser.parse_args()
+
+  process(args.inputfile)
+
+      
