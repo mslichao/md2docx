6ef28b9872e3c819ee047a192169d89ae1bfb107
Chao Li (MSRA) <lchao@microsoft.com>
2019-11-07 22:55:24 +0800

    center title

--- batchmd2docx.py
+++ batchmd2docx.py
@@ -4,6 +4,8 @@ import sys
 import subprocess
 from typing import Iterator
 from helper import makesurePandoc
+import json
+import re
 
 def find_all_files(directory: str, follow_symlinks=False) -> Iterator[str]:
     for entry in os.scandir(directory):
@@ -46,11 +48,43 @@ def process(inputdir, outputdir, ignore_custom_size):
       outputfile=os.path.join(outputdir, relpathWithoutExt+'.docx')
       os.makedirs(os.path.dirname(outputfile), exist_ok=True)
       
-      pandocCmdList = [pandoc, '--dpi', '160', '--filter', 'customfilter.py', '--reference-doc', 'customref.docx', inputfile, '-o', outputfile, '--resource-path', os.path.dirname(inputfile)]
+      # pandocCmdList = [pandoc, '--dpi', '160', '--filter', 'customfilter.py', '--reference-doc', 'customref.docx', inputfile, '-o', outputfile, '--resource-path', os.path.dirname(inputfile)]
+      pandocCmdList = [pandoc, '--filter', 'customfilter.py', inputfile, '-t', 'json']
       if not ignore_custom_size:
         pandocCmdList.append('--filter')
         pandocCmdList.append('customsizefilter.py')
-      subprocess.call(pandocCmdList)
+
+      jsonString = subprocess.run(pandocCmdList, stdout=subprocess.PIPE).stdout
+      originDoc = json.loads(jsonString)
+      originBlocks = originDoc['blocks']
+
+      tempBlocks = originBlocks[:]
+      for (index, item) in enumerate(tempBlocks):
+        try:
+          if item['t'] == 'Table':
+            preItemPara = tempBlocks[index - 1]
+            if preItemPara['t'] == 'Para' and re.match(r'表\d+-\d+', preItemPara['c'][0]['c']):
+              if len(item['c'][0]) == 0:
+                item['c'][0].extend(preItemPara['c'])
+                originBlocks.remove(preItemPara)
+          elif item['t'] == 'Para' and item['c'][0]['t'] == 'Image':
+            nextItemPara = tempBlocks[index + 1]
+            if nextItemPara['t'] == 'Para' and re.match(r'图\d+-\d+', nextItemPara['c'][0]['c']):
+              image = item['c'][0]['c']
+              if not image[2][1]:
+                image[2][1] = 'fig:'
+                image[1].clear()
+                image[1].extend(nextItemPara['c'])
+                originBlocks.remove(nextItemPara)
+        except Exception:
+          pass
+
+      # for item in originBlocks:
+      #   print(item)
+
+      jsonString = json.dumps(originDoc)
+      pandocWriter = [pandoc, '-f', 'json', '--dpi', '160', '--reference-doc', 'customref.docx', '-o', outputfile, '--resource-path', os.path.dirname(inputfile)]
+      subprocess.run(pandocWriter, input=bytes(jsonString, encoding='utf-8'))
       subprocess.call([sys.executable, 'fixEqNum.py', outputfile])
       subprocess.call([sys.executable, 'fixImageCenter.py', outputfile])
 
